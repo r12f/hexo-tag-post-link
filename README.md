19e3331a1213ff47a3ed940a9159aeb7837a181a
r12f <r12f.code@gmail.com>
2017-02-03 09:19:00 -0800

    Initial checkin.

---
+++ README.md
@@ -0,0 +1,54 @@
+# hexo-tag-post-link
+
+[![npm version](https://badge.fury.io/js/hexo-tag-post-link.svg)](http://badge.fury.io/js/hexo-tag-post-link)
+
+A hexo tag plugin for inserting post link in your posts with your own format.
+
+## Installation
+
+``` bash
+$ npm install hexo-tag-post-link --save
+```
+
+## Usage
+
+1. Create "post_link.ymal" in your "_source/_data" folder
+2. Add your own post link format in ymal
+   ```
+   name: format
+   ```
+
+   Example
+   ```
+   header: <b>The link to this post: </b><a href="<%= post_permalink %>" target="_blank"><%= post_title %></a>
+   ```
+
+3. Add post link tag to wherever you like in your post
+   ```
+   {% post_link <name> %}
+   ```
+   Example:
+   ```
+   {% post_link header %}
+   ```
+
+## Supported Variables:
+
+* site_title
+* site_subtitle
+* site_description
+* site_author
+* site_url
+* post_title
+* post_created
+* post_created_date
+* post_created_time
+* post_updated
+* post_updated_date
+* post_updated_time
+* post_relative_url
+* post_permalink
+
+## License
+
+BSD v3
---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var underscore = require('underscore');
+
+var configKeys = {
+    'site_title' : (config) => config.title,
+    'site_subtitle' : (config) => config.subtitle,
+    'site_description' : (config) => config.description,
+    'site_author' : (config) => config.author,
+    'site_url' : (config) => config.url
+};
+
+var postKeys = {
+    'post_title' : (config) => config.title,
+    'post_created' : (config) => config.date,
+    'post_created_date' : (config) => config.date.format(hexo.config.date_format),
+    'post_created_time' : (config) => config.date.format(hexo.config.time_format),
+    'post_updated' : (config) => config.updated,
+    'post_updated_date' : (config) => config.updated.format(hexo.config.date_format),
+    'post_updated_time' : (config) => config.updated.format(hexo.config.time_format),
+    'post_relative_url' : (config) => config.path,
+    'post_permalink' : (config) => config.permalink
+};
+
+function createTemplateArgs(postInfo) {
+    var templateArgs = {};
+
+    Object.keys(configKeys).forEach((key) => { templateArgs[key] = configKeys[key](hexo.config); });
+    Object.keys(postKeys).forEach((key) => { templateArgs[key] = postKeys[key](postInfo); });
+
+    return templateArgs;
+}
+
+hexo.extend.tag.register('post_link', function (args, content) {
+    var templateName = args[0];
+
+    var postLinkTemplates = hexo.locals.get('data').post_link;
+    if (!postLinkTemplates || !postLinkTemplates[templateName]) {
+        return '';
+    }
+
+    var template = postLinkTemplates[templateName];
+    var templateArgs = createTemplateArgs(this);
+    return underscore.template(template)(templateArgs);
+})
